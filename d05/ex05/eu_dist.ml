2133af4348e1c5ace7273468b2a20f21e32ee1a8
Fernan Morales <Atelier-Morales>
2016-01-12 08:46:14 +0100

    ex05

---
+++ d05/ex05/eu_dist.ml
@@ -0,0 +1,38 @@
+let eu_dist a b =
+  let n = Array.length a in
+  if n <> Array.length b then
+	failwith "Arrays have different lengths";
+  if n = 0 then
+	failwith "Arrays are empty !!";
+  let square (acc, i) av =
+	let bv = b.(i) in
+	((av -. bv) ** 2. +. acc, i + 1)
+  in
+  let (squaredsum, _) = Array.fold_left square (0., 0) a in
+  sqrt squaredsum
+
+
+let test a b =
+  Printf.printf "Test with: \n%!";
+  Printf.printf "a: [|%!";
+  Array.iter (fun v -> print_string ((string_of_float v) ^ "; ")) a;
+  Printf.printf "|]\nb: [|%!";
+  Array.iter (fun v -> print_string ((string_of_float v) ^ "; ")) b;
+  Printf.printf "|]\n";
+  Printf.printf "eu_dist a b = %!";
+  try
+	Printf.printf "%.f%!\n" (eu_dist a b)
+  with
+  | Failure m
+	->	Printf.printf "Catched \"%s\" :(\n%!" m
+					  
+
+let () =
+  test [||] [||];
+  test [|1.|] [||];
+  test [|1.|] [|1.|];
+  test [|1.; 1.|] [|1.; 1.|];
+  test [|42.|] [|-.42.|];
+  test [|-42.|] [|42.|];
+  test [|45.; 98456.; 9856.; 84.; 8946.; 89.; 8945.|]
+	   [|5.; 9856.; 986.; 4.; 846.; 8.; 895.|];
